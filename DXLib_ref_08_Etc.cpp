6a4ca025ef9f556c231928329effda14523eec02
emiya_ubw <a6m3.zero.f@gmail.com>
2024-11-06 22:31:59 +0900

    ada

--- DXLib_ref_08_Etc.cpp
+++ DXLib_ref_08_Etc.cpp
@@ -177,7 +177,7 @@ namespace DXLibRef {
 		}
 		bool DrawControl::IsDrawOnWindow(int x1, int y1, int x2, int y2) noexcept {
 			auto* DrawParts = DXDraw::Instance();
-			return HitRectangleToRectangle(0, 0, DrawParts->GetScreenXMax(), DrawParts->GetScreenYMax(), std::min(x1, x2), std::min(y1, y2), std::max(x1, x2), std::max(y1, y2));
+			return HitRectangleToRectangle(0, 0, DrawParts->GetUIXMax(), DrawParts->GetUIYMax(), std::min(x1, x2), std::min(y1, y2), std::max(x1, x2), std::max(y1, y2));
 		}
 		// ” 
 		void SetBox(int xp1, int yp1, int xp2, int yp2, unsigned int colorSet) noexcept {
